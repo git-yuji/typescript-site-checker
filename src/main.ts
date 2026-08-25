ecc3bf350f3b5f69cf889fd60c6207596466429b
yujihozumi <yuu.60011@gmail.com>
2026-08-25 17:36:21 +0900

    feat: URL入力フォームを追加

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,40 @@ app.innerHTML = `
     <p class="eyebrow">TYPESCRIPT LEARNING</p>
     <h1>Webサイト公開前チェッカー</h1>
     <p>ここから、ひとつずつチェック機能を作っていきます。</p>
+
+    <form id="url-form">
+      <label for="url-input">WebサイトのURL</label>
+
+      <input
+        type="text"
+        id="url-input"
+        name="url"
+        placeholder="https://example.com"
+      >
+
+      <button type="submit">チェックする</button>
+    </form>
   </main>
 `;
 
+const urlForm = document.querySelector<HTMLFormElement>("#url-form");
+
+if (urlForm === null) {
+  throw new Error("#url-form が見つかりません。");
+}
+
+const urlInput = document.querySelector<HTMLInputElement>("#url-input");
+
+if (urlInput === null) {
+  throw new Error("#url-input が見つかりません。");
+}
+
+const handleSubmit = (event: SubmitEvent): void => {
+  event.preventDefault();
+
+  const inputValue = urlInput.value;
+
+  console.log(inputValue);
+};
+
+urlForm.addEventListener("submit", handleSubmit);
